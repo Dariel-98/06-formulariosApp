b61ef249ca0211966dc4aab7307d53062bad2b8b
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-21 08:29:48 -0600

    Template clean form

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,35 @@ import { NgForm } from '@angular/forms';
 export class BasicosComponent {
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'Rtx 3080',
+    precio: 10,
+    existencias: 10,
+  };
+
   guardar() {
-    console.log(this.miFormulario);
+    // console.log(this.miFormulario);
+    console.log('Posteo correcto');
+    this.miFormulario.resetForm({ precio: 0, existencias: 0 });
   }
 
   nombreValido(): boolean {
     return (
-      this.miFormulario?.form.controls['producto']['invalid'] &&
+      this.miFormulario?.form.controls['producto']?.invalid &&
       this.miFormulario?.form.controls['producto']['touched']
+    );
+  }
+
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls['precio']?.value < 0 &&
+      this.miFormulario.controls['precio']['touched']
+    );
+  }
+  existenciasValido(): boolean {
+    return (
+      this.miFormulario?.controls['existencias']?.value < 1 &&
+      this.miFormulario.controls['existencias']['touched']
     );
   }
 }
