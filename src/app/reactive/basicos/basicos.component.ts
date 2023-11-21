77f1295839ef3f89eca03fd428e827587e51fd14
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-21 13:14:26 -0600

    Setting up Reactive Forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent {
-
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTX 4080ti'),
+  });
+  constructor() {}
 }
