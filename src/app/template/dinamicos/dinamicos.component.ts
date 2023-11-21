7ded02794ff2563fcadeeb0d9c2274071b51c59c
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-21 10:04:13 -0600

    Adding elements in Dinamic form

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Dariel',
+    favoritos: [
+      { id: 1, nombre: 'Dota' },
+      { id: 2, nombre: 'World of Warcraft' },
+      { id: 3, nombre: 'StarCraft' },
+    ],
+  };
 
+  guardar() {
+    console.log('Formulario posteado');
+  }
 }
