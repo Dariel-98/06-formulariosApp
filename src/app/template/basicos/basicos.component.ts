e1b95d1f16f32ee1007c236fb36e81c8225c55ab
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-20 21:51:33 -0600

    Template show Error message

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -8,6 +8,6 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent {
   guardar(miFormulario: NgForm) {
-    console.log(miFormulario.value);
+    console.log(miFormulario.form.controls['producto']['invalid']);
   }
 }
