8d9cb3ca3dce433a45c9bff02fc0a206dab4e77b
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-11-20 21:29:10 -0600

    Template FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { TemplateModule } from './template/template.module';
+import { ReactiveModule } from './reactive/reactive.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    ReactiveModule,
+    SharedModule,
+    TemplateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { SwitchesComponent } from './switches/switches.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 
-
 @NgModule({
-  declarations: [
-    BasicosComponent,
-    SwitchesComponent,
-    DinamicosComponent
-  ],
-  imports: [
-    CommonModule,
-    TemplateRoutingModule
-  ]
+  declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
+  imports: [CommonModule, FormsModule, TemplateRoutingModule],
 })
-export class TemplateModule { }
+export class TemplateModule {}
