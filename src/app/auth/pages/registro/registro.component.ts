e6b2d6e1b8b93fd4ecc46ee4ee946fde3712e641
Dariel <gomez.b.dariel@gmail.com>
2023-11-22 07:42:21 -0600

    Personalized Validations

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -11,12 +16,21 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-z]+)';
   emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
+  noPuedeSerStrider(control: FormControl) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if (valor === 'strider') {
+      return { noStrider: true };
+    }
+    return null;
+  }
+
   miFormulario: FormGroup = this.fb.group({
     nombre: [
       '',
       [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
     ],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username: ['', [Validators.required, this.noPuedeSerStrider]],
   });
 
   constructor(private fb: FormBuilder) {}
@@ -25,6 +39,7 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.reset({
       nombre: 'Dariel Gomez',
       email: 'test1@test.com',
+      username: 'dariel_98',
     });
   }
   campoNoValido(campo: string) {
